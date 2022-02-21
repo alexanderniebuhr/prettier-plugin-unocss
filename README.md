9c4d39ef089a8a4f2c0eadb73e91b749403cc7c1
Alexander Niebuhr <tech@nbhr.io>
2022-02-21 09:26:05 +0100

    chore: :tada: first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center">Welcome to @alexanderniebuhr/prettier-plugin-unocss 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/alexanderniebuhr/prettier-plugin-unocss#readme" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://github.com/alexanderniebuhr/prettier-plugin-unocss/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" />
+  </a>
+  <a href="https://github.com/alexanderniebuhr/prettier-plugin-unocss/blob/master/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/alexanderniebuhr/@alexanderniebuhr/prettier-plugin-unocss" />
+  </a>
+</p>
+
+### 🏠 [Homepage](https://github.com/alexanderniebuhr/prettier-plugin-unocss)
+
+## Author
+
+👤 **Alexander Niebuhr**
+
+* Github: [@alexanderniebuhr](https://github.com/alexanderniebuhr)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/alexanderniebuhr/prettier-plugin-unocss/issues). You can also take a look at the [contributing guide](https://github.com/alexanderniebuhr/prettier-plugin-unocss/blob/master/CONTRIBUTING.md).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2022 [Alexander Niebuhr](https://github.com/alexanderniebuhr).<br />
+This project is [MIT](https://github.com/alexanderniebuhr/prettier-plugin-unocss/blob/master/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/index.js
@@ -0,0 +1,108 @@
+// const resolvedConfig = resolveConfig(config)
+// const presets = resolvedConfig.presets
+// function mergePresets<T extends 'rules'>(key: T, presets, config): Required<UserConfig>[T] {
+//   return uniq([
+//     ...presets.flatMap(p => toArray(p[key] || []) as any[]),
+//     ...toArray(config[key] || []) as any[],
+//   ])
+// }
+
+// const rules = mergePresets('rules', resolvedConfig.presets, config)
+
+import prettierParserHTML from 'prettier/parser-html'
+import presetUno from '@unocss/preset-uno'
+
+const rules = presetUno().rules
+
+// let check = 'bg-gray-400'
+// console.log(
+//   rules.findIndex((rule) => {
+//     if (typeof rule[0] == 'string') {
+//       return new RegExp(rule[0]).test(check)
+//     } else {
+//       return rule[0].test(check)
+//     }
+//   })
+// )
+
+export const options = {}
+
+function sortUtillities(a, b) {
+  if (a.includes(':')) return 1
+  if (b.includes(':')) return -1
+
+  //for each index and value of rules array
+  let aSortIndex = 0
+  let bSortIndex = 0
+
+  for (let [index, value] of rules.entries()) {
+    if (typeof value[0] === 'string') {
+      if (new RegExp(value[0], 'g').test(a)) aSortIndex = index
+      if (new RegExp(value[0], 'g').test(b)) bSortIndex = index
+    } else {
+      if (value[0].test(a)) aSortIndex = index
+      if (value[0].test(b)) bSortIndex = index
+    }
+  }
+
+  return bSortIndex < aSortIndex ? 1 : -1
+}
+
+export const parsers = {
+  html: {
+    astFormat: prettierParserHTML.parsers.html.astFormat,
+    parse: prettierParserHTML.parsers.html.parse,
+    locStart: prettierParserHTML.parsers.html.locStart,
+    locEnd: prettierParserHTML.parsers.html.locEnd,
+    preprocess: (text, options) => {
+      // global regex to replace all classes with their order
+      const regex = /(?<=class=")(.*?)(?=")/g
+      const result = text.replace(regex, (match) => {
+        const groups = []
+        const groupRegex = /([!\w][\w:_/-]*?):\(([\w\s/-]*?)\)/g
+        const utils = match.replace(groupRegex, (group, variant, utillities) => {
+          groups.push(`${variant}:(${utillities.split(' ').sort(sortUtillities).join(' ')})`)
+          return ''
+        })
+        return utils.split(' ').sort(sortUtillities).join(' ') + ' ' + groups.join(' ')
+      })
+
+      return result
+    },
+  },
+  // vue: {
+  //   astFormat:prettierParserHTML.parsers.vue.astFormat,
+  //   parse: prettierParserHTML.parsers.vue.parse,
+  //   locStart: prettierParserHTML.parsers.vue.locStart,
+  //   locEnd:  prettierParserHTML.parsers.vue.locEnd,
+  //   preprocess: (text, options) => {
+
+  //     // global regex to replace all classes with their order
+  //     const regex = /(?<=class=")(.*?)(?=")/g
+  //     const result = text.replace(regex, (match) => {
+  //       return "needs to be sorted"
+  //     })
+
+  //     return result
+  //   },
+  // },
+  // svelte: {
+  //   astFormat:prettierParserHTML.parsers.html.astFormat,
+  //   parse: prettierParserHTML.parsers.html.parse,
+  //   locStart: prettierParserHTML.parsers.html.locStart,
+  //   locEnd:  prettierParserHTML.parsers.html.locEnd,
+  //   preprocess: (text, options) => {
+
+  //     // global regex to replace all classes with their order
+  //     const regex = /(?<=class=")(.*?)(?=")/g
+  //     const result = text.replace(regex, (match) => {
+  //       return match.split(" ").sort().join(" ")
+  //     })
+
+  //     return result
+  //   },
+  // },
+  // css: createParser(prettierParserPostCSS.parsers.css, transformCss),
+  // scss: createParser(prettierParserPostCSS.parsers.scss, transformCss),
+  // less: createParser(prettierParserPostCSS.parsers.less, transformCss),
+}
